761544e4fdb577ebc15bbf79ccf0bc099002ce32
ericsherman4 <esherman2018@gmail.com>
2021-12-11 19:14:06 -0500

    i think sim works

--- sim.py
+++ sim.py
@@ -4,7 +4,6 @@
 import struct
 import string
 import sys
-import binascii
 import random
 
 
@@ -15,7 +14,7 @@ class gvars:
     SIZE_L2 = 10
     SIZE_TLB_ENTRIES = 32
     SIZE_OFFSET = 12
-    VPN_MASK = (2 ** (32-SIZE_OFFSET) -1) << SIZE_OFFSET
+    VPN_MASK = ((2 ** (32-SIZE_OFFSET)) -1) << SIZE_OFFSET
 
 
 
@@ -26,6 +25,7 @@ class TLB:
     num_entries = 0
     num_hits = 0
     hit_rate = 1
+    num_misses = 0
 
 
 def LRU_update(RU_idx, num_entries):
@@ -51,6 +51,7 @@ def TLB_action(i,data):
     if miss:
         # print("miss")
         TLB.hit_rate = TLB.num_hits / i
+        TLB.num_misses += 1
         #if TLB is full, use LRU
         if TLB.num_entries == gvars.SIZE_TLB_ENTRIES:
             # use bit doesnt change, num entries doesnt change
@@ -60,11 +61,9 @@ def TLB_action(i,data):
             for k in range(0, gvars.SIZE_TLB_ENTRIES):
                 if TLB.last_used[k] >= max_val:
                     idx = k
-            print(idx)
+            # print("THINGT HERE" + str(idx) + "\n\n")
             LRU_update(idx, TLB.num_entries)
             TLB.entries[k] = data[1]
-
-
         else:
             # find the first empty spot
             for idx in range(0, gvars.SIZE_TLB_ENTRIES):
@@ -83,53 +82,52 @@ def main():
 
     file = open(gvars.FILE_NAME, "r")
     contents = file.readlines()
+    file.close()
 
     i = 0
+    length = len(contents)
+    increment_add = int(length / 25)
+    percentage_add = int(100/25)
+    increment = increment_add
+    percentage = percentage_add
 
-    while( i < len(contents)):
-    # while( i < 2):
+    while( i < length):
+    # while( i < 300000):
         data = [int(contents[i][0:1]),int(contents[i][2:-1],16)]
+        # data[1] = (data[1] & gvars.VPN_MASK) >> gvars.SIZE_OFFSET
         data[1] = (data[1] & gvars.VPN_MASK) >> gvars.SIZE_OFFSET
         # print(TLB_entries)
         TLB_action(i+1,data)
         # print(TLB.last_used)
-        
 
-        # print(data[1])
-        # print(type(data[1]))
-        # print(len(data[1]))
+        if i == increment: 
+            print("Percent complete: " + str(percentage) + "%")
+            increment += increment_add
+            percentage += percentage_add
 
-        if data[0] == 0:
-            # read
-            pass
-        elif data[0] == 1:
-            # write
-            pass
-        elif data[0] == 2:
-            # instruction fetch
-            pass
-        elif data[0] == 3:
-            # misc
-            pass
-        else:
-            # default
-            print("Instruction not valid. Exiting...")
-            sys.exit()
+
+        # if data[0] == 0:
+        #     # read
+        #     pass
+        # elif data[0] == 1:
+        #     # write
+        #     pass
+        # elif data[0] == 2:
+        #     # instruction fetch
+        #     pass
+        # elif data[0] == 3:
+        #     # misc
+        #     pass
+        # else:
+        #     # default
+        #     print("Instruction not valid. Exiting...")
+        #     sys.exit()
         
         i+=1
 
     print("Final hit rate: " + str(TLB.hit_rate*100)[0:9] + "%")
-
-# TLB is for translating virtual address to physical address
-# we are given the virtual address
-# on a miss, we need to go through the page table and get the physical address, but the assingment says not to worry about the that. 
-# soo, on a hit or a miss, what exactly are we putting iin the the TLB???????
-    
-
-
-
-    file.close()
-
+    print("Total misses: " + str(TLB.num_misses))
+    print("Total hits: " + str(TLB.num_hits))
 
 
 if __name__ == "__main__":
